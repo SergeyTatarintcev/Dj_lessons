48a0e6d10cd92f7d8067f38667444349f86d131a
Сергей <Serg18-124@yandex.ru>
2025-08-04 15:16:27 +0300

    push

--- zerocod/main/views.py
+++ zerocod/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Это мой первый проект на Django </h1> .")
+    return render(request, 'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1> Это вторая страница на Django </h1> .")
+    return render(request, 'main/new.html')
