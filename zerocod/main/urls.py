da1d01c54a5abd21886a43d1a6d9f4944f6e603b
Сергей <Serg18-124@yandex.ru>
2025-08-05 15:27:28 +0300

    Закончил учебный сайт на Django

--- zerocod/main/urls.py
+++ zerocod/main/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('',     views.index, name='index'),
-    path('new/', views.new,   name='new'),
+    path('',         views.index,    name='home'),
+    path('new/',     views.new,      name='page2'),
+    path('about/',   views.about,    name='about'),
+    path('contacts/',views.contacts, name='contacts'),
+    path('team/',    views.team,     name='team'),
+    path('jobs/',    views.jobs,     name='jobs'),
+    path('faq/',     views.faq,      name='faq'),
 ]
--- zerocod/main/views.py
+++ zerocod/main/views.py
@@ -1,8 +1,23 @@
+# zerocod/main/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'caption': 'CapyDjango'})
 
 def new(request):
     return render(request, 'main/new.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+def team(request):
+    return render(request, 'main/team.html')
+
+def jobs(request):
+    return render(request, 'main/jobs.html')
+
+def faq(request):
+    return render(request, 'main/faq.html')
